1d4160bc3e594a7f36d98eaf3f540c1d20907bd6
Darius Pažusinskis <dariuk.p@gmail.com>
2022-03-21 04:13:52 +0200

    second task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetsComponent } from './budgets/budgets.component';
+import { ExpensesItemComponent } from './expenses-item/expenses-item.component';
 import { ExpensesComponent } from './expenses/expenses.component';
+import { FilterExpenseGuard } from './filter-expense.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'expenses', component: ExpensesComponent },
+  {
+    path: 'expense/:id',
+    component: ExpensesItemComponent,
+    canActivate: [FilterExpenseGuard],
+  },
   { path: 'budgets', component: BudgetsComponent },
 ];
 
