56336311f8e13ed3d74cf5185a531f0d00c98dcf
Darius Pažusinskis <dariuk.p@gmail.com>
2022-03-21 10:52:11 +0200

    task-3 updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BudgetsComponent } from './budgets/budgets.component';
-import { ExpensesItemComponent } from './expenses-item/expenses-item.component';
+import { ExpenseDetailsComponent } from './expense-details/expense-details.component';
 import { ExpensesComponent } from './expenses/expenses.component';
 import { FilterExpenseGuard } from './filter-expense.guard';
 import { HomeComponent } from './home/home.component';
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'expenses', component: ExpensesComponent },
   {
     path: 'expense/:id',
-    component: ExpensesItemComponent,
+    component: ExpenseDetailsComponent,
     canActivate: [FilterExpenseGuard],
   },
   { path: 'budgets', component: BudgetsComponent },
