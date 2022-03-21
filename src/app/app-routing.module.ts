5c9a75fc0f90d8c9a9f3425abc43988ab7d28720
Darius Pažusinskis <dariuk.p@gmail.com>
2022-03-21 04:00:22 +0200

    first task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BudgetsComponent } from './budgets/budgets.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'expenses', component: ExpensesComponent },
+  { path: 'budgets', component: BudgetsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
