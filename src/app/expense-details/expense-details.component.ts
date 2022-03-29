93f80456a0f404df1fb5616ed07751c323a1c2f3
Brigita <97016501+brimac2@users.noreply.github.com>
2022-03-29 21:42:52 +0300

    feat: template driven form without validation

--- src/app/expense-details/expense-details.component.ts
+++ src/app/expense-details/expense-details.component.ts
@@ -1,15 +1,27 @@
+import { ExpensesService } from 'src/app/expenses.service';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { observable, of, Observable } from 'rxjs';
+import { Expense } from '../shared/expense';
 
 @Component({
   selector: 'app-expense-details',
   templateUrl: './expense-details.component.html',
-  styleUrls: ['./expense-details.component.css']
+  styleUrls: ['./expense-details.component.css'],
 })
 export class ExpenseDetailsComponent implements OnInit {
-
-  constructor() { }
+  expense$: Observable<Expense> = of();
+  id: string | null = '';
+  constructor(
+    private expensesService: ExpensesService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      this.expense$ = this.expensesService.getExpense(this.id);
+    }
   }
-
 }
