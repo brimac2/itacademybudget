69218d731c0b8f471498b84beacdd1d7a0f7b5c3
Brigita <97016501+brimac2@users.noreply.github.com>
2022-04-04 11:28:15 +0300

    fix: clean code

--- src/app/budgets/budgets.component.ts
+++ src/app/budgets/budgets.component.ts
@@ -1,29 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-/**
- * Nors componento tiesiogiai ir nerašei, bet vienas tip for the future, kaip palaikyt cleaner code. Čia gali išimti `styleUrls` ir tada ištrint css failą, jei nenaudojamas.
- * Toliau gali ištrinti constructor ir ngOnInit ir kartu implements OnInit ir jo importą. Taip komponentas atrodys daug paprasčiau.
- */
+
 @Component({
-  selector: 'app-budgets',
-  templateUrl: './budgets.component.html',
-  styleUrls: ['./budgets.component.css']
-})
-export class BudgetsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
-
-//example:
-/*
-import { Component } from '@angular/core';
-
- @Component({
   selector: 'app-budgets',
   templateUrl: './budgets.component.html',
 })
 export class BudgetsComponent {}
-*/
